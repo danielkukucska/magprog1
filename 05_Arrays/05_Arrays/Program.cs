46aee5dc49046b8e64ea0650b2a15e5272f140f7
Daniel Kukucska <daniel.kukucska.txt@gmail.com>
2022-10-12 21:42:22 +0200

    Arrays, randoms

---
+++ 05_Arrays/05_Arrays/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ATM();
+
+
+            //int[] grades = { 1, 3, 4, 2, 2 };
+
+            //double avg = grades.Average();
+
+            //int gradesAboveAvg = 0;
+            //Console.WriteLine("Grades:");
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //    if (grade > avg)
+            //        gradesAboveAvg++;
+            //}
+
+            //Console.WriteLine($"Average: {avg:0.00}");
+            //Console.WriteLine($"Grades above average: {gradesAboveAvg}");
+
+            //Console.ReadLine();
+
+            //Random rnd = new Random();
+            //int[] nums = new int[10];
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    nums[i] = rnd.Next(100);
+            //}
+            //Console.WriteLine(string.Join(", ", nums));
+
+            //string str = "test text";
+            //char[] charArray = str.ToCharArray();
+            //Array.Reverse(charArray);
+
+            ////string(charArray)
+            //Console.WriteLine(charArray);
+
+            CreateDistincArray();
+        }
+
+        static void ATM()
+        {
+            int[] denominations = new int[] { 20000, 10000, 5000, 2000, 1000, 500 };
+            int[] outOfStockDenominations = new int[] { 1000, 2000 };
+            Console.Write("Amount: ");
+            int amount = int.Parse(Console.ReadLine());
+
+            foreach (int denomiation in denominations)
+            {
+                if (outOfStockDenominations.Contains(denomiation))
+                    continue;
+
+                int count = amount / denomiation;
+                if (count > 0)
+                {
+                    Console.WriteLine($"{count} * {denomiation} Ft");
+                    amount -= count * denomiation;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void CreateDistincArray()
+        {
+            int[] nums = new int[100];
+
+            int index = 0;
+            Random rnd = new Random();
+            while (index < nums.Length)
+            {
+                int num = rnd.Next(100);
+
+                bool exists = false;
+                for (int i = 0; i < index; i++)
+                {
+                    if (nums[i] == num)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    nums[index] = num;
+                    index++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",nums));
+        }
+    }
+}
---
+++ 06_Randoms/06_Randoms/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Randoms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //always one initialization
+            Random random = new Random();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+                //[0;int.MaxValue)
+                Console.WriteLine(random.Next());
+
+                //[0;100)
+                Console.WriteLine(random.Next(100));
+
+                //[-10;100)
+                Console.WriteLine(random.Next(-10,100));
+
+                //[-10;100) even numbers
+                Console.WriteLine(random.Next(-5, 50)*2);
+
+                //[3;20) .000
+                Console.WriteLine(random.Next(3*1000, 20*1000)/1000.0);
+
+                //[0;1)
+                Console.WriteLine(random.NextDouble());
+
+                //[3;20) double
+                //Math.Round
+                Console.WriteLine(random.NextDouble() * (20 - 3) +3);
+            //}
+        }
+    }
+}
